e1b193e0866b65a246249e48f6232a60d6931eb9
KeiyaYamaguchi <yamakei.28@icloud.com>
2025-06-03 18:46:02 +0900

    correct handler

--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module line-news-bot
 
 go 1.24.1
 
-require github.com/joho/godotenv v1.5.1
-
-require github.com/aws/aws-lambda-go v1.48.0 // indirect
+require github.com/aws/aws-lambda-go v1.48.0
--- src/main.go
+++ src/main.go
@@ -2,25 +2,32 @@ package main
 
 import (
 	"fmt"
+	"context"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func main() {
-	// TODO : パスをハードコーディングしたくない
-	// err := godotenv.Load("../.env")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
+func handler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	response := events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "\"Hello from Lambda!\"",
+	}
 
 	news, err := CallNewsAPI()
 	if err != nil {
 		fmt.Println(err)
-		return
+		return response, err
 	}
 
 	err = CallLineAPI(news)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return response, err
 	}
+
+	return response, nil
+}
+
+func main() {
+	lambda.Start(handler)
 }
